7152c7c0999c098d8e01be3d8e206d43f8d66165
Buskemor <130304167+Buskemor@users.noreply.github.com>
2024-01-31 14:55:52 +0100

    visual candy

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ let userInput = document.getElementById("numberInput");
 let correctAnswer = 0;
 
 let randomNr = 0;
-randomNr = generateEquation()
-displayEquationElem.innerHTML = randomNr
+randomNr = generateEquation();
+displayEquationElem.innerHTML = randomNr;
 
 correctAnswer = parseInt(randomNr[0]) + parseInt(randomNr[4])
 // console.log(correctAnswer)
 
 let previousStartTime = 0;
-let deltaTimeArr = []
-let repeatEquationsArr = []
-let roundsUntilRepeatArr = []
+let deltaTimeArr = [];
+let repeatEquationsArr = [];
+let roundsUntilRepeatArr = [];
 
 let wrongCounter = 0;
 let correctCounter = 0;
@@ -32,25 +32,25 @@ let correctCounter = 0;
 userInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         if (userInput.value > 18 || userInput.value == '') {
-            previousEquation.innerHTML = "type number under 19"
-            userInput.value = ""
+            previousEquation.innerHTML = "type number under 19";
+            userInput.value = "";
             return;
         }
-        wrongCounter++
-        correctCounter++
+        wrongCounter++;
+        correctCounter++;
 
-        timeAlgorythm()
+        timeAlgorythm();
 
         checkUserInput();
 
         if (checkBoxElem.checked) {
-            updateDisplayWithAlgorythm()
+            updateDisplayWithAlgorythm();
         }
-        randomNr = generateEquation()
-        updateDisplay()
+        randomNr = generateEquation();
+        updateDisplay();
     } else {
         if (isNaN(event.key)) {
-            event.preventDefault()
+            event.preventDefault();
         }
     }
 });
@@ -58,24 +58,24 @@ userInput.addEventListener("keypress", function(event) {
 
 function timeAlgorythm() {
 
-    let startTime = new Date().getTime()
+    let startTime = new Date().getTime();
     let deltaTime = startTime - previousStartTime;
     previousStartTime = startTime;
     if (deltaTime > 8000) { //ASSUME USER HAS BEEN AFK
         if (deltaTime === startTime) {
-            deltaTime = 1000
+            deltaTime = 1000;
         }
         deltaTime = averageDeltaTime(deltaTime, deltaTimeArr);
     }
     if (deltaTime > (averageDeltaTime(deltaTime, deltaTimeArr) + 1000) || userInput.value != correctAnswer) {
-        repeatEquationsArr.push(randomNr[0], randomNr[4])
-        roundsUntilRepeatArr.push(3 + Math.ceil(Math.random()*7))
+        repeatEquationsArr.push(randomNr[0], randomNr[4]);
+        roundsUntilRepeatArr.push(3 + Math.ceil(Math.random()*7));
     }
     // console.log(roundsUntilRepeatArr)
     // deltaTimeArr.push(deltaTime)
     // console.log(deltaTimeArr)
-    console.log(averageDeltaTime(deltaTime, deltaTimeArr))
-}
+    console.log(averageDeltaTime(deltaTime, deltaTimeArr));
+};
 
 function averageDeltaTime(deltaTime, deltaTimeArr) {
     let sumOfDeltaTime = 0;
@@ -83,52 +83,52 @@ function averageDeltaTime(deltaTime, deltaTimeArr) {
         sumOfDeltaTime += deltaTimeArr[i];
     }
     return averageTime = (deltaTime + sumOfDeltaTime) / (deltaTimeArr.length+1);
-}
+};
 
 function checkUserInput() {
     userInput = document.getElementById("numberInput");
     if (userInput.value == correctAnswer) {
         correctOrIncorrectElem.innerHTML = `Correct ${correctCounter}x`
         wrongCounter = 0;
-        playSound('kenneth1.mp3')
-        previousEquation.innerHTML = "wp"
+        playSound('kenneth1.mp3');
+        previousEquation.innerHTML = "wp";
     } else {
-        correctOrIncorrectElem.innerHTML = `Incorrect ${wrongCounter}x`
+        correctOrIncorrectElem.innerHTML = `Incorrect ${wrongCounter}x`;
         correctCounter = 0;
-        playSound('kenneth2.mp3')
-        previousEquation.innerHTML = `(${randomNr[0]} + ${randomNr[4]} = ${correctAnswer}) ≠ ${userInput.value}`
+        playSound('kenneth2.mp3');
+        previousEquation.innerHTML = `(${randomNr[0]} + ${randomNr[4]} = ${correctAnswer}) ≠ ${userInput.value}`;
     }
-}
+};
 
 function updateDisplayWithAlgorythm() {
     for (let i = 0; i < roundsUntilRepeatArr.length; i++) {
-        roundsUntilRepeatArr[i]--
+        roundsUntilRepeatArr[i]--;
         if (roundsUntilRepeatArr[i] === 0) {
-            randomNr = `${repeatEquationsArr[1]} + ${repeatEquationsArr[0]}` //switch them around so it feels different
-            updateDisplay()
-            roundsUntilRepeatArr.splice(i, 1)
-            repeatEquationsArr.splice(0, 2)
+            randomNr = `${repeatEquationsArr[1]} + ${repeatEquationsArr[0]}`; //switch them around so it feels different
+            updateDisplay();
+            roundsUntilRepeatArr.splice(i, 1);
+            repeatEquationsArr.splice(0, 2);
             return;
         }
     }
-}
+};
 
 function playSound(mp3) {
-    let audio = new Audio(mp3)
-    audio.play()
-}
+    let audio = new Audio(mp3);
+    audio.play();
+};
 
 function generateEquation() {
-    return `${Math.ceil(Math.random()*9)} + ${Math.ceil(Math.random()*9)}`
-}
+    return `${Math.ceil(Math.random()*9)} + ${Math.ceil(Math.random()*9)}`;
+};
 
 function focusInput() {
     document.getElementById("numberInput").focus();
-}
+};
 
 function updateDisplay() {
-    displayEquationElem.innerHTML = randomNr
-    correctAnswer = parseInt(randomNr[0]) + parseInt(randomNr[4]) //parseint because it's a string xd
-    console.log(correctAnswer)
-    userInput.value = ""
-}
+    displayEquationElem.innerHTML = randomNr;
+    correctAnswer = parseInt(randomNr[0]) + parseInt(randomNr[4]); //parseint because it's a string xd
+    console.log(correctAnswer);
+    userInput.value = "";
+};
